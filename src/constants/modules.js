e5a01a0b9d7c05da904257727a1c061ab3b0d2ea
irankunda-patient <irankundapatien@gmail.com>
2024-09-19 02:03:20 -0700

    v2

---
+++ src/constants/modules.js
@@ -0,0 +1,17 @@
+// export const modules = {
+//     [
+//         {"monday":["Nosql", "Franch", "Machine-Learning"]},
+//         {"tuesday":["Math","Blockchain", "Flutter"]},
+//         {"wednesday":["Python","Ent-ship"]},
+//         {"thursday":["Ent-ship","French","Machine-Learning"]},
+//         {"friday":["Flutter","Math","Python"]}
+//     ]
+// }
+
+export const modules = [
+  { names: ["Nosql", "Franch", "Machine-Learning"], day: "monday" },
+  { names: ["Math", "Blockchain", "Flutter"], day: "tuesday" },
+  { names: ["Python", "Ent-ship"], day: "wednesday" },
+  { names: ["Ent-ship", "French", "Machine-Learning"], day: "thursday" },
+  { names: ["Flutter", "Math", "Python"], day: "friday" },
+];
