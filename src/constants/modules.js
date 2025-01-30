1b99764753b06ae2c3e6c50028ca05ce3706462c
ipcode-cloud <ipcode0@gmail.com>
2025-01-30 23:18:31 +0200

    [update] for second term #2025

--- src/constants/modules.js
+++ src/constants/modules.js
@@ -1,3 +1,5 @@
+
+
 // export const modules = {
 //     [
 //         {"monday":["Nosql", "Franch", "Machine-Learning"]},
@@ -7,11 +9,19 @@
 //         {"friday":["Flutter","Math","Python"]}
 //     ]
 // }
-
+// _______1st Term_____________
+// export const modules = [
+//   { names: ["Nosql", "E-ship", "Machine-Learning"], day: "Monday" },
+//   { names: ["Math", "Blockchain", "Flutter"], day: "Tuesday" },
+//   { names: ["Nosql", "pw"], day: "Wednesday" },
+//   { names: ["Blockchain", "reactJs", "Python"], day: "Thursday" },
+//   { names: ["Machine-learning", "Math", "Flutter"], day: "friday" },
+// ];
+// ________2nd Term_____________
 export const modules = [
-  { names: ["Nosql", "E-ship", "Machine-Learning"], day: "Monday" },
-  { names: ["Math", "Blockchain", "Flutter"], day: "Tuesday" },
-  { names: ["Nosql", "pw"], day: "Wednesday" },
-  { names: ["Blockchain", "reactJs", "Python"], day: "Thursday" },
-  { names: ["Machine-learning", "Math", "Flutter"], day: "friday" },
+  { names: ["Nosql", "Mathematics", "Machine-Learning"], day: "Monday" },
+  { names: ["MobileApp", "Blockchain", "Reactjs"], day: "Tuesday" },
+  { names: ["Nosql", "English"], day: "Wednesday" },
+  { names: ["M-Learning", "ReactJs", "Python"], day: "Thursday" },
+  { names: ["Blockchain", "Physics", "MobileApp"], day: "Friday" },
 ];
